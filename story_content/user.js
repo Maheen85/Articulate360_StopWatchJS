267732da9253b51741a91d8f8e04b27317ee4e2b
Maheen Shaikh <Shaikh.maheen@gmail.com>
2023-07-17 16:21:27 -0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6LtSCGgz2T6":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  //Getting the player
+const player = GetPlayer();
+
+//Where to start
+let sec = 0;
+
+//Set up the timer
+function startTimer(){
+	sec += 1;
+	player.SetVar("timer",sec);
+	if (sec== 100){
+		clearInterval(timerId);
+	}
+}
+
+//Start the timer
+timerId = setInterval(startTimer, 1000);
+}
+
