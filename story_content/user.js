37a3cba52506255748144ce6effdf9ca247a85c7
Maheen Shaikh <Shaikh.maheen@gmail.com>
2023-07-30 21:54:39 -0700

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,30 +2,112 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5gn1bhgmqUL":
+      case "5y2m0TeYRhl":
         Script1();
+        break;
+      case "6A79c5A1mC2":
+        Script2();
+        break;
+      case "67boCBPysdC":
+        Script3();
         break;
   }
 }
 
 function Script1()
 {
-  //Getting the player
+  
+//* CODE # 01
+
+
+//Getting the player
 const player = GetPlayer();
 
+
+let dummy = player.GetVar("timerDisplay_pv");
+
+player.SetVar("secondsOnTimer_pv",dummy );
+player.SetVar("stopTimer_pv","True");
+
+
+
+
+clearInterval(timerId);
+
+
+}
+
+function Script2()
+{
+  	//* CODE # 01
+
+
+//Getting the player
+const player = GetPlayer();
+
+
+clearInterval(timerId);
+
 //Where to start
-let sec = 0;
+let secondsOnTimer = 0;
+player.SetVar("secondsOnTimer_pv",0);
+
 
 //Set up the timer
 function startTimer(){
-	sec += 1;
-	player.SetVar("timer",sec);
-	if (sec == 12){
+	secondsOnTimer += 1;
+	player.SetVar("timerDisplay_pv",secondsOnTimer);
+	if (secondsOnTimer == 25){
 		clearInterval(timerId);
+		player.SetVar("timerComplete_pv","True");
 	}
 }
 
 //Start the timer
 timerId = setInterval(startTimer, 1000);
+
+// END CODE # 01 */
+
+}
+
+function Script3()
+{
+  
+//Getting the player
+const player = GetPlayer();
+
+//Where to start
+let secondsOnTimer = player.GetVar("secondsOnTimer_pv");
+
+
+//Set up the timer
+function startTimer(){
+	secondsOnTimer += 1;
+	player.SetVar("timerDisplay_pv",secondsOnTimer);
+	
+
+	if (secondsOnTimer == 45){
+		clearInterval(timerId);
+		player.SetVar("timerComplete_pv","True");
+	}
+}
+
+//Start the timer
+
+	timerId = setInterval(startTimer, 1000);
+	player.SetVar("startTimer_pv","True");
+	
+
+	
+// END CODE # 01 */
+
+
+
+
+
+
+
+
+
 }
 
